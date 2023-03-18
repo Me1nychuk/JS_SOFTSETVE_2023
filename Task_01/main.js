6af00e43cd6e5963a0652b455943378554037fea
Sviatoslav <svyatoslav200104@gmail.com>
2023-03-18 16:30:11 +0200

    Add first task.

---
+++ Task_01/main.js
@@ -0,0 +1,18 @@
+const year = prompt("Enter your year of birth");
+alert(`Your age: ${2023 - year}`);
+//==========================================================================
+radius = prompt("Enter the radius of the circle");
+alert(`Area of a circle: ${(2*Math.PI*radius).toFixed(2)}`);
+//==========================================================================
+distanse = prompt("Enter the distance between the 2 points");
+time = prompt("Enter the time you have");
+alert(`You have to move with speed - ${(distanse/time).toFixed(2)}`);
+//==========================================================================
+const euro = 0.92;
+money =prompt("Enter the dollar amount");
+money *= euro;
+alert(`you will get - ${money.toFixed(2)}`); 
+//==========================================================================
+amount_of_memory = prompt("Specify the storage volume");
+amount_of_memory *= 1024;
+alert(`you can download ${(amount_of_memory/820).toFixed(0)} files`)
