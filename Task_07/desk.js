a225571497aaf48e1df8414cedbf7b5c6fe8a9c2
Sviatoslav <svyatoslav200104@gmail.com>
2023-04-14 17:56:52 +0300

    Update Task_07.

--- Task_07/desk.js
+++ Task_07/desk.js
@@ -4,7 +4,7 @@ const ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
 for (let suit of suits){
     for(let rank of ranks){
-        desk.push(rank+suit);
+        desk.push(rank+" "+suit);
     }
 }
 
