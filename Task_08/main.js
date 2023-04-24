715c80011c804ed272c6eecd7b024eb24eda8509
Sviatoslav <svyatoslav200104@gmail.com>
2023-04-24 20:52:14 +0300

    Edit Task_8.

--- Task_08/main.js
+++ Task_08/main.js
@@ -3,8 +3,8 @@ const cars = [];
 const tableBody = document.querySelector(`tbody`);
 
 function AddNewCar(car) {
-    tableBody.innerHTML += `<tr>
-                                <th scope="col">${car.id}</th>
+  tableBody.innerHTML += `<tr>
+                                <td scope="col">${car.id}</th>
                                 <td scope="col">${car.model}</td>  
                                 <td scope="col">${car.type}</td>                           
                                 <td scope="col">${car.year}</td>
@@ -15,95 +15,88 @@ function AddNewCar(car) {
 </tr>`;
 }
 
-function ReadCar(){
-    const modelInput = document.getElementById(`modelInput`).value;
-    const yearInput = document.getElementById(`yearInput`).value;
-    const priceInput = document.getElementById(`priceInput`).value;
-    const typeInput = document.getElementById(`typeInput`).value;
-    const customsClearanceInput = document.getElementById(`statusInput`);
-    const customsClearanceI =customsClearanceInput.checked ? "TRUE" : "FALSE";
-    const ddate = NewDateSell();
-    return {
-        id: cars.length + 1,
-        model: modelInput,
-        year: yearInput,
-        price: priceInput,
-        type: typeInput,
-        customsClearance: customsClearanceI,
-        date: ddate
-    };
+function ReadCar() {
+  const modelInput = document.getElementById(`modelInput`).value;
+  const yearInput = document.getElementById(`yearInput`).value;
+  const priceInput = document.getElementById(`priceInput`).value;
+  const typeInput = document.getElementById(`typeInput`).value;
+  const customsClearanceInput = document.getElementById(`statusInput`);
+  const customsClearanceI = customsClearanceInput.checked ? "TRUE" : "FALSE";
+  const ddate = NewDateSell();
+  return {
+    id: cars.length + 1,
+    model: modelInput,
+    year: yearInput,
+    price: priceInput,
+    type: typeInput,
+    customsClearance: customsClearanceI,
+    date: ddate
+  };
 }
-function NewDateSell(){
-    const date =new Date();
-    const year = date.getFullYear().toString();
-    const month = date.getMonth().toString();
-    const day = date.getDay().toString();
-    const hours = date.getHours().toString().padStart(2,'0');
-    const minutes = date.getMinutes().toString().padStart(2,'0');
-    const seconds = date.getSeconds().toString().padStart(2,'0');
-    const dateForSell = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    return dateForSell;
+function NewDateSell() {
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  const month = date.getMonth().toString();
+  const day = date.getDay().toString();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const dateForSell = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return dateForSell;
 }
 
 const addBtn = document.getElementById('add-car-btn');
 
 addBtn.onclick = (event) => {
-    event.preventDefault();
-    const newCar = ReadCar();
-    if (!newCar.model || !newCar.year || !newCar.price || !newCar.type) {
-        alert("Please fill in all required fields.");
-        return;
-    }
-    cars.push(newCar);
-    AddNewCar(newCar);
-    ChangeColor();
-    
+  event.preventDefault();
+  const newCar = ReadCar();
+  if (!newCar.model || !newCar.year || !newCar.price || !newCar.type) {
+    alert("Please fill in all required fields.");
+    return;
+  }
+  cars.push(newCar);
+  AddNewCar(newCar);
+  ChangeColor();
+
 }
 
-function ChangeColor(){
-    const color = document.getElementById("colorInput").value;
-    const colorDiv = document.getElementById(`colorId${cars.length}`);
-    colorDiv.style.backgroundColor = color;
+function ChangeColor() {
+  const color = document.getElementById("colorInput").value;
+  const colorDiv = document.getElementById(`colorId${cars.length}`);
+  colorDiv.style.backgroundColor = color;
 }
 
 const ClearBtn = document.getElementById(`clear-all-btn`);
 
-ClearBtn.onclick = () =>{
-    tableBody.innerHTML = "";
+ClearBtn.onclick = () => {
+  tableBody.innerHTML = "";
 }
 
-/*
-const SearchBtn = document.getElementById(`searchInput`);
 
-SearchBtn.onclick = () => {
-    const searchInput = document.getElementById(`searchInput`).value.toLowerCase().trim();
-    const rows = document.querySelectorAll('tbody tr');
-    let cheker = false;
-    let i;
-
-    for ( i = 0; i < rows.length; i++) {
-        const columns = rows[i].querySelectorAll(`td`);
-        let cheker = false;
-      
-      
-          if (columns[1].textContent.toLowerCase().indexOf(searchInput) !== -1) {
-            cheker = true;
-           
-          }
-        
-      
-        if (cheker) {
-          rows[i].style.display = '';
-        } else {
-          rows[i].style.display = 'none';
-        }
+document.querySelector(`#search`).oninput = function () {
+  let val = this.value.trim();
+  let tableSearch = document.querySelectorAll(`tbody tr td`);
+  if (val != "") {
+    tableSearch.forEach(function (elem) {
+      if (elem.innerText.search(val) == -1) {
+          elem.classList.add('hide');
+          elem.innerHTML = elem.innerText
+      } else {
+          elem.classList.remove(`hide`);
+          let str = elem.innerTEXT;
+          elem.innerHTML = searchMark(str,elem.innerText.search(val),val.length);
       }
-      
-
-    if (!cheker) {
-        alert('No results found.');
-    }
+    });
+  } else {
+    tableSearch.forEach(function (elem) {
+      elem.classList.remove('hide');
+      elem.innerHTML = elem.innerText;
+    })
+  }
 }
-*/
 
+function searchMark(string, pos, len) {
+  return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+
+  }
 
